abd6deed70e2610d1ed3e225a8cb7c804fa2baa5
Gracjan Koscinski <gracek247927@gmail.com>
2024-12-22 17:40:52 +0100

    init

---
+++ terraform/main.tf
@@ -0,0 +1,85 @@
+resource "azurerm_resource_group" "myterraformgroup" {
+  name     = var.resource_group_name
+  location = var.location
+}
+
+resource "azurerm_virtual_network" "myterraformvnet" {
+  name                = var.vnet_name
+  address_space       = [var.vnet_address_space]
+  location            = azurerm_resource_group.myterraformgroup.location
+  resource_group_name = azurerm_resource_group.myterraformgroup.name
+}
+
+resource "azurerm_subnet" "myterraformsubnet" {
+  name                 = var.subnet_name
+  resource_group_name  = azurerm_resource_group.myterraformgroup.name
+  virtual_network_name = azurerm_virtual_network.myterraformvnet.name
+  address_prefixes     = [var.subnet_address_prefix]
+}
+
+resource "azurerm_container_registry" "acr" {
+  name                = var.acr_name
+  resource_group_name = azurerm_resource_group.myterraformgroup.name
+  location            = azurerm_resource_group.myterraformgroup.location
+  sku                 = "Basic"
+  admin_enabled       = true
+}
+
+resource "azurerm_postgresql_flexible_server" "postgres" {
+  name                = "postgres-medfast"
+  resource_group_name = azurerm_resource_group.myterraformgroup.name
+  location            = azurerm_resource_group.myterraformgroup.location
+  version             = "16"
+  
+  administrator_login    = var.db_username
+  administrator_password = var.db_password
+
+  storage_mb = 32768
+  sku_name   = "B_Standard_B1ms"
+
+  backup_retention_days = 7
+}
+
+resource "azurerm_postgresql_flexible_server_database" "medfast" {
+  name      = "medfast"
+  server_id = azurerm_postgresql_flexible_server.postgres.id
+  charset   = "UTF8"
+  collation = "en_US.utf8"
+}
+
+resource "azurerm_postgresql_flexible_server_firewall_rule" "allow_azure_services" {
+  name             = "allow-azure-services"
+  server_id        = azurerm_postgresql_flexible_server.postgres.id
+  start_ip_address = "0.0.0.0"
+  end_ip_address   = "0.0.0.0"
+}
+
+resource "azurerm_kubernetes_cluster" "aks" {
+  name                = var.aks_cluster_name
+  location            = azurerm_resource_group.myterraformgroup.location
+  resource_group_name = azurerm_resource_group.myterraformgroup.name
+  dns_prefix          = "medfast-k8s"
+  kubernetes_version  = "1.29.2"
+
+  default_node_pool {
+    name            = "default"
+    node_count      = var.aks_node_count
+    vm_size         = var.aks_node_vm_size
+    os_disk_size_gb = 30
+    vnet_subnet_id  = azurerm_subnet.myterraformsubnet.id
+  }
+
+  identity {
+    type = "SystemAssigned"
+  }
+}
+
+resource "azurerm_role_assignment" "acr_pull" {
+  principal_id         = azurerm_kubernetes_cluster.aks.kubelet_identity[0].object_id
+  role_definition_name = "AcrPull"
+  scope                = azurerm_container_registry.acr.id
+}
+
+output "postgres_host" {
+  value = "${azurerm_postgresql_flexible_server.postgres.name}.postgres.database.azure.com"
+}
